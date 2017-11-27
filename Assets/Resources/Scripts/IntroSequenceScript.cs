f51b9bea80a8ff1d2e767abc8eb9aae8a0ca49ac
KazzTruul <kazztruul@hotmail.com>
2017-11-27 11:35:19 +0100

    introsequence fixed

---
+++ Assets/Resources/Scripts/IntroSequenceScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroSequenceScript : MonoBehaviour {
+
+    [SerializeField]
+    Canvas canvas;
+
+    [SerializeField]
+    float timer;
+
+    void Start()
+    {
+        StartCoroutine("DestroyMe");
+    }
+    
+    IEnumerator DestroyMe()
+    {
+        yield return new WaitForSeconds(timer);
+        canvas.gameObject.SetActive(true);
+        Destroy(this.gameObject);
+    }
+}
